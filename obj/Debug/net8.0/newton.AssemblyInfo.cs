4bd26e9c091a9f40705f1c81b0668458b445f454
youssef abouelenien <kgayoussefbs@gmail.com>
2024-11-30 16:49:21 +0100

    kontktxtes

--- obj/Debug/net8.0/newton.AssemblyInfo.cs
+++ obj/Debug/net8.0/newton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae97fed0404354423ef443f024315e46508e5663")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e689a52454ee3cf2128d47cead69aa453ffa4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("newton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
